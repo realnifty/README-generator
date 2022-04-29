4cf98464949edc601ea2749e43495c561a63a3ef
gordo <rogelioalbertopedraza@gmail.com>
2022-04-29 17:32:09 -0500

    question array + included packages

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter installation instructions for your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter usage instructions for your project.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please enter contribution guidelines for your project.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please enter testing instructions for your project.'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
