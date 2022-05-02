1770435b496cee77b9438237cb2bbbbb478491e3
realnifty <rogelioalbertopedraza@gmail.com>
2022-05-02 02:21:03 -0500

    renderLicenseSection complete

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -32,11 +32,50 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+  else if (license === 'GNU AGPLv3') {
+    return `[${license}](https://spdx.org/licenses/AGPL-3.0-or-later.html)`;
+  }
+  else if (license === 'GNU GPLv3') {
+    return `[${license}](https://spdx.org/licenses/GPL-3.0-or-later.html)`;
+  }
+  else if (license === 'GNU LGPLv3') {
+    return `[${license}](https://spdx.org/licenses/LGPL-3.0-or-later.html)`;
+  }
+  else if (license === 'Mozilla Public License 2.0') {
+    return `[${license}](https://spdx.org/licenses/MPL-2.0.html)`;
+  }
+  else if (license === 'Apache License 2.0') {
+    return `[${license}](https://spdx.org/licenses/Apache-2.0.html)`;
+  }
+  else if (license === 'MIT License') {
+    return `[${license}](https://spdx.org/licenses/MIT.html)`;
+  }
+  else if (license === 'Boost Software License 1.0') {
+    return `[${license}](https://spdx.org/licenses/BSL-1.0.html)`;
+  }
+  else if (license === 'The Unlicense') {
+    return `[${license}](https://spdx.org/licenses/Unlicense.html)`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  else if (license) {
+    return `
+    ## License
+
+    Licensed under the [${license}](${renderLicenseLink(license)})
+    `;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
